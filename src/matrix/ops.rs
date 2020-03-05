90c3f16bb960fa0f4e6ed044a3aa45d52cd72ecc
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-03-05 18:44:52 +0100

    small doc for matrix

--- src/matrix/ops.rs
+++ src/matrix/ops.rs
@@ -11,6 +11,25 @@ use std::ops::{
 impl<T> Mul<Matrix<T>> for Matrix<T> {
     type Output = Matrix<T>;
 
+    /// Std Ops Multiplication implementation for Matrix struct
+    ///
+    /// perform dot product for each combination of row and column
+    /// and save in new instance
+    ///
+    /// # Arguments 
+    ///
+    /// * `lhs` - current matrix
+    /// * `rhs` - other matrix
+    ///
+    /// # Example 
+    ///
+    /// ```
+    ///
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// If two matrices don't match
     fn mul(self, rhs: Matrix<T>) -> Self::Output {
         assert!(self.cols == rhs.rows);
 
@@ -22,15 +41,11 @@ impl<T> Mul<Matrix<T>> for Matrix<T> {
 
                 for row in 0..self.rows {
                     for col in 0..rhs.cols {
-                        // dot product of row and col
-                        let acc = self.get_row(row).unwrap()
+                        data.push(self.get_row(row).unwrap()
                             .zip(rhs.get_col(col).unwrap())
-                            .map(|(a, b)| *a * *y).sum();
-
-                        data.push(acc);
+                            .map(|(a, b)| *a * *y).sum());
                     }
                 }
-
                 data
             },
         }
