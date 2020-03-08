c4abce7a8202cef0f97614adae2dc0ef6f2b02d0
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-03-08 15:09:33 +0100

    implementing json parser

---
+++ src/json_parser.rs
@@ -0,0 +1,13 @@
+mod multilayer;
+
+use multilayer::MultiLayerPerceptron as mlp;
+
+
+pub struct Parser {
+    config: &str,
+}
+
+
+impl Parser {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,12 @@
-mod layer;
-mod matrix;
+mod json_parser;
 
-use layer::Layer;
-use matrix::Matrix;
+use std::env::args;
+use json_parser::Parser;
 
-// Just not to get warnings for now
+
 fn main() {
-    let layer = Layer::new(4, 4, "relu".to_string());
+    let args: Vec<String> = args().collect();
 
-    let matrix: Matrix<f64> = Matrix::new(10, 10);
-    assert!(matrix.rows() == 10 && matrix.cols() == 10);
-
-    let transposed = matrix.transpose();
-
-    let mut data = vec![vec![]];
-    for col in 0..10 {
-        data.push(
-            matrix
-                .get_col(col)
-                .unwrap()
-                .cloned()
-                .collect::<Vec<f64>>()
-                .clone(),
-        );
-    }
-
-    let mat1 = vec![vec![1.0], vec![1.0]];
-    let mat2 = vec![vec![1.0, 2.0, 3.0], vec![1.0, 2.0, 3.0]];
-
-    let mat = Matrix::from(mat2);
-
-    println!("{:?}", mat);
-    println!("{:?}", Matrix::from(mat1).transpose() * mat);
-    println!("{:?}", transposed);
+    let fcnn = Parser::new(&args[1]);
+    fcnn.train();
 }
